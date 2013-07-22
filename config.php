4ec432a57fa066d063e73f053ebd5de436c3e225
Julian <julian.dinges@gmail.com>
2013-07-22 15:32:23 +0200

    Added DbConnector and removed database connection details from config.php

--- config.php
+++ config.php
@@ -3,9 +3,4 @@ if (!defined('ITVERWALTUNG')) die();
 
 define('HOME_DIR', $_SERVER['DOCUMENT_ROOT'] . '/ITVerwaltung/');
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASSWORD', '');
-
 ?>
---
+++ lib/DbConnector.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Class for database connection.
+ * 
+ * @author Thunraz
+ */
+
+class DbConnector
+{
+	private $db;
+
+	private function __construct()
+	{
+		$DbHost = "";
+		$DbName = "";
+		$DbUser = "";
+		$DbPass = "";
+
+		// Open connection to mysql database (using PDO)
+		$this->db = new PDO(
+			sprintf("mysql:host=%s;dbname=%s;charset=utf8", $DbHost, $DbName),
+			$DbUser,
+			$DbPass,
+			array(
+				// Do not emulate prepared statements
+				// (see: http://stackoverflow.com/questions/10113562/pdo-mysql-use-pdoattr-emulate-prepares-or-not)
+				PDO::ATTR_EMULATE_PREPARES => false,
+
+				// If an error occurs, throw an exception.
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+			)
+		);
+	}
+
+	public static function getInstance()
+	{
+		static $instance = null;
+		if($instance === null)
+		{
+			$instance = new DbConnect();
+		}
+
+		return $instance;
+	}
+}
+
+?>
