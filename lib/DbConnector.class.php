6f480751801195791b56f3a428226f0189f2087b
AlexSal <alexander.salbaum@web.de>
2013-07-24 15:18:45 +0200

    Added Room methods to DbConnector.class

--- lib/DbConnector.class.php
+++ lib/DbConnector.class.php
@@ -5,6 +5,48 @@
  * 
  * @author Thunraz <julian.dinges@gmail.com>
  */
+
+// define management information attributes for DB
+define ('DB_MANAGE_VALID', 'vwi_valid');
+define ('DB_MANAGE_CREATED', 'vwi_created');
+define ('DB_MANAGE_LASTUPDATED', 'vwi_lastupdated');
+
+// define user attributes for DB
+define('DB_USER_ID', 'pk_ben_id');
+define('DB_USER_NAME', 'ben_name');
+define('DB_USER_PWD', 'ben_pwdhash');
+define('DB_USER_CREATE_DATE', 'ben_erstellungsdatum');
+
+// define supplier attributes for DB
+define ('DB_SUPPLIER_ID', 'pk_l_id');
+define ('DB_SUPPLIER_COMPANYNAME', 'l_firmenname');
+define ('DB_SUPPLIER_STREET', 'l_strasse');
+define ('DB_SUPPLIER_ZIPCODE', 'l_plz');
+define ('DB_SUPPLIER_CITY', 'l_ort');
+define ('DB_SUPPLIER_PHONE', 'l_tel');
+define ('DB_SUPPLIER_MOBILE', 'l_mobil');
+define ('DB_SUPPLIER_FAX', 'l_fax');
+define ('DB_SUPPLIER_EMAIL', 'l_email');
+
+// define room attributes for DB
+define ('DB_ROOM_ID', 'pk_r_id');
+define ('DB_ROOM_NUMBER', 'r_nr');
+define ('DB_ROOM_NAME', 'r_bezeichnung');
+define ('DB_ROOM_NOTE', 'r_notiz');
+
+// define component attributes for DB
+define ('DB_COMPONENT_ID', 'pk_k_id');
+define ('DB_COMPONENT_SUPPLIER', 'fk_l_k_lieferid');
+define ('DB_COMPONENT_ROOM', 'fk_r_komp_kompraum');
+define ('DB_COMPONENT_PURCHASE_DATE', 'k_einkaufsdatum');
+define ('DB_COMPONENT_WARRANTY_PERIOD', 'k_gewaehrleistungsdatum');
+define ('DB_COMPONENT_NOTICE', 'k_notiz');
+define ('DB_COMPONENT_MANUFACTURER', 'k_hersteller');
+
+// define module attributes for DB
+define ('DB_MODULE_ID', 'pk_mod_id');
+define ('DB_MODULE_NAME', 'mod_name');
+
 class DbConnector
 {
 	private $db;
@@ -51,9 +93,12 @@ class DbConnector
 	public function getUsers()
 	{
 		$query = "SELECT ";
-		$query .= "benutzer.ben_name ";
+		$query .= DB_USER_ID . " ";
+		$query .= DB_USER_NAME . " ";
+		$query .= DB_USER_PWD . " ";
+		$query .= DB_USER_CREATE_DATE . " ";
 		$query .= "FROM benutzer ";
-		$query .= "WHERE benutzer.vwi_valid = 1";
+		$query .= "WHERE " . DB_MANAGE_VALID . " = 1";
 				
 		$statement = $this->db->query($query);
 	
@@ -80,12 +125,11 @@ class DbConnector
 			return false;
 		}
 	
-		$query = "UPDATE ";
-		$query .= "  Benutzer ";
-		$query .= "SET (benutzer.pwd_hash = :password ";
-		$query .= ", benutzer.vwi_lastupdated = sysdate()) ";
-		$query .= "WHERE ben_name = :user ";
-		$query .= "AND benutzer.vwi_valid = 1";
+		$query = "UPDATE Benutzer ";
+		$query .= "SET (" . DB_USER_PWD . " = :password ";
+		$query .= ", vwi_lastupdated = sysdate()) ";
+		$query .= "WHERE " .DB_USER_NAME . " = :user ";
+		$query .= "AND " . DB_MANAGE_VALID . " = 1";
 	
 		$statement = $this->db->prepare($query);
 		$statement->bindparam(':user', $user);
@@ -108,10 +152,10 @@ class DbConnector
 		}
 	
 	$query = "SELECT ";
-	$query .= "	(benutzer.ben_pwdhash) ";
+	$query .= "	(" . DB_USER_PWD . ") ";
 	$query .= "FROM benutzer ";
-	$query .= "WHERE benutzer.ben_name = :user ";
-	$query .= "AND benutzer.vwi_valid = 1";
+	$query .= "WHERE " . DB_USER_NAME . " = :user ";
+	$query .= "AND " . DB_MANAGE_VALID . " = 1";
 	
 	$statement = $this->db->prepare($query);
 	$statement->bindparam(':user', $user);
@@ -136,28 +180,28 @@ class DbConnector
 	public function updateSupplier(Supplier $Supplier)
 	{
 		$query = "UPDATE lieferant ";
-		$query .= "SET l.firmenname = :Companyname ";
-		$query .= ", l_strasse = :Street ";
-		$query .= ", l_plz = :Zipcode ";
-		$query .= ", l_ort = :City ";
-		$query .= ", l_tel = :Phone ";
-		$query .= ", l_mobil = :Mobile ";
-		$query .= ", l_fax = :Fax ";
-		$query .= ", l_email = :Email ";
-		$query .= ", vwi_lastupdated = sysdate() ";
-		$query .= "WHERE pk_l_id = :Id ";
-		$query .= "AND vwi_valid = 1";
+		$query .= "SET " . DB_SUPPLIER_COMPANYNAME . " = :companyname ";
+		$query .= ", " . DB_SUPPLIER_STREET . " = :street ";
+		$query .= ", " . DB_SUPPLIER_ZIPCODE . "= :zipcode ";
+		$query .= ", " . DB_SUPPLIER_CITY . " = :city ";
+		$query .= ", " . DB_SUPPLIER_PHONE . " = :phone ";
+		$query .= ", " . DB_SUPPLIER_MOBILE . " = :mobile ";
+		$query .= ", " . DB_SUPPLIER_FAX . " = :fax ";
+		$query .= ", " . DB_SUPPLIER_EMAIL . " = :email ";
+		$query .= ", " . DB_MANAGE_LASTUPDATED . " = sysdate() ";
+		$query .= "WHERE " . DB_SUPPLIER_ID . " = :id ";
+		$query .= "AND " . DB_MANAGE_VALID . " = 1";
 		
 		$statement = $this->db->prepare($query);
-		$statement->bindparam(':Companyname', $Supplier->companyname);
-		$statement->bindparam(':Street', $Supplier->street);
-		$statement->bindparam(':Zipcode', $Supplier->zipcode);
-		$statement->bindparam(':City', $Supplier->city);
-		$statement->bindparam(':Phone', $Supplier->phone);
-		$statement->bindparam(':Mobile', $Supplier->mobile);
-		$statement->bindparam(':Fax', $Supplier->fax);
-		$statement->bindparam(':Email', $Supplier->email);
-		$statement->bindparam(':Id', $Supplier->id);
+		$statement->bindparam(':companyname', $Supplier->getCompanyname());
+		$statement->bindparam(':street', $Supplier->getStreet());
+		$statement->bindparam(':zipcode', $Supplier->getZipcode());
+		$statement->bindparam(':city', $Supplier->getCity());
+		$statement->bindparam(':phone', $Supplier->getPhone());
+		$statement->bindparam(':mobile', $Supplier->getMobile());
+		$statement->bindparam(':fax', $Supplier->getFax());
+		$statement->bindparam(':email', $Supplier->getEmail());
+		$statement->bindparam(':id', $Supplier->getId());
 		$statement->execute();
 		
 		if ($query == false)
@@ -174,10 +218,10 @@ class DbConnector
 	public function deleteSupplier(Supplier $Supplier)
 	{
 		$query = "UPDATE lieferant ";
-		$query .= "SET vwi_valid = 0";
-		$query .= ", vwi_lastupdated = sysdate() ";
-		$query .= "WHERE pk_l_id = :Id ";
-		$query .= "AND vwi_valid = 1";
+		$query .= "SET " . DB_MANAGE_VALID . " = 0";
+		$query .= ", " . DB_MANAGE_LASTUPDATED . " = sysdate() ";
+		$query .= "WHERE " . DB_SUPPLIER_ID . " = :id ";
+		$query .= "AND " . DB_MANAGE_VALID . " = 1";
 	
 		$statement = $this->db->prepare($query);
 		$statement->bindparam(':Id', $Supplier->id);
@@ -196,15 +240,15 @@ class DbConnector
 	public function createSupplier(Supplier $Supplier)
 	{
 		$query = "INSERT INTO lieferant ";
-		$query .= "(l_firmenname ";
-		$query .= "l_strasse ";
-		$query .= "l_plz ";
-		$query .= "l_ort ";
-		$query .= "l_tel ";
-		$query .= "l_mobil ";
-		$query .= "l_fax ";
-		$query .= "l_email ";
-		$query .= "l_created) ";
+		$query .= "(" .DB_SUPPLIER_COMPANYNAME . " ";
+		$query .= "" . DB_SUPPLIER_STREET . " ";
+		$query .= "" . DB_SUPPLIER_ZIPCODE . " ";
+		$query .= "" . DB_SUPPLIER_CITY . " ";
+		$query .= "" . DB_SUPPLIER_PHONE . " ";
+		$query .= "" . DB_SUPPLIER_MOBILE . " ";
+		$query .= "" . DB_SUPPLIER_FAX . " ";
+		$query .= "" . DB_SUPPLIER_EMAIL . " ";
+		$query .= "" . DB_MANAGE_CREATED . ") ";
 		$query .= "VALUES (:Companyname ";
 		$query .= ", :Street ";
 		$query .= ", :Zipcode ";
@@ -214,19 +258,126 @@ class DbConnector
 		$query .= ", :Fax ";
 		$query .= ", :Email ";
 		$query .= ", sysdate()) ";
-		$query .= "WHERE pk_l_id = :Id ";
+		$query .= "WHERE " . DB_SUPPLIER_ID . " = :id ";
 		$query .= "AND vwi_valid = 1";
 	
 		$statement = $this->db->prepare($query);
-		$statement->bindparam(':Companyname', $Supplier->companyname);
-		$statement->bindparam(':Street', $Supplier->street);
-		$statement->bindparam(':Zipcode', $Supplier->zipcode);
-		$statement->bindparam(':City', $Supplier->city);
-		$statement->bindparam(':Phone', $Supplier->phone);
-		$statement->bindparam(':Mobile', $Supplier->mobile);
-		$statement->bindparam(':Fax', $Supplier->fax);
-		$statement->bindparam(':Email', $Supplier->email);
-		$statement->bindparam(':Id', $Supplier->id);
+		$statement->bindparam(':companyname', $Supplier->getCompanyname());
+		$statement->bindparam(':street', $Supplier->getStreet());
+		$statement->bindparam(':zipcode', $Supplier->getZipcode());
+		$statement->bindparam(':city', $Supplier->getCity());
+		$statement->bindparam(':phone', $Supplier->getPhone());
+		$statement->bindparam(':mobile', $Supplier->getMobile());
+		$statement->bindparam(':fax', $Supplier->getFax());
+		$statement->bindparam(':email', $Supplier->getEmail());
+		$statement->bindparam(':id', $Supplier->getId());
+		$statement->execute();
+	
+		if ($query == false)
+			return false;
+	}
+	
+	/**
+	 * gets nodule id.
+	 *
+	 * @param string $name
+	 * @return false if an error occurs, otherwise true.
+	 */
+	public function getModuleId(string $name)
+	{
+		if ($name == null)
+		{
+			return false;
+		}
+		
+		$query = "SELECT ";
+		$query .= "" . DB_MODULE_ID . " ";
+		$query .= "FROM module ";
+		$query .= "WHERE " . DB_MODULE_NAME . " = :name";
+		
+		$statement = $this->db->prepare($query);
+		$statement->bindparam(':name', $name);
+		$statement->execute();
+		
+		$result = array();
+		$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+		
+		if($result == false)
+			return false;
+		
+		return $result;
+		
+	}
+	
+	/**
+	 * Updates Component informations.
+	 *
+	 * @param ???
+	 * @return false if an error occurs, otherwise true.
+	 */
+	public function updateComponent(Component $Component)
+	{
+		$query = "UPDATE komponenten ";
+		$query .= "SET " . DB_COMPONENT_SUPPLIER. " = :supplier ";
+		$query .= ", " . DB_COMPONENT_ROOM . " = :room ";
+		$query .= ", " . DB_COMPONENT_PURCHASE_DATE . "= :purchaseDate ";
+		$query .= ", " . DB_COMPONENT_WARRANTY_PERIOD . " = :warrantyPeriod ";
+		$query .= ", " . DB_COMPONENT_NOTICE . " = :notice ";
+		$query .= ", " . DB_COMPONENT_MANUFACTURER. " = :manufacturer ";
+		$query .= ", " . DB_MANAGE_LASTUPDATED . " = sysdate() ";
+		$query .= "WHERE " . DB_SUPPLIER_ID . " = :id ";
+		$query .= "AND " . DB_MANAGE_VALID . " = 1";
+	
+		$statement = $this->db->prepare($query);
+		$statement->bindparam(':companyname', $Component->getCompanyname());
+		$statement->bindparam(':street', $Component->getStreet());
+		$statement->bindparam(':zipcode', $Component->getZipcode());
+		$statement->bindparam(':city', $Component->getCity());
+		$statement->bindparam(':phone', $Component->getPhone());
+		$statement->bindparam(':mobile', $Component->getMobile());
+		$statement->bindparam(':fax', $Component->getFax());
+		$statement->bindparam(':email', $Component->getEmail());
+		$statement->bindparam(':id', $Component->getId());
+		$statement->execute();
+	
+		if ($query == false)
+			return false;
+	}
+	
+	/**
+	 * Creates Component informations.
+	 *
+	 * @param ???
+	 * @return false if an error occurs, otherwise true.
+	 */
+	public function createComponent(Component $Component)
+	{
+		$query = "INSERT INTO komponenten ";
+		$query .= "" . DB_COMPONENT_SUPPLIER . " ";
+		$query .= ", " . DB_COMPONENT_ROOM . " ";
+		$query .= ", " . DB_COMPONENT_PURCHASE_DATE . " ";
+		$query .= ", " . DB_COMPONENT_WARRANTY_PERIOD . " ";
+		$query .= ", " . DB_COMPONENT_NOTICE . " ";
+		$query .= ", " . DB_COMPONENT_MANUFACTURER . " ";
+		$query .= ", " . DB_MANAGE_CREATED . " ";
+		$query .= "VALUES (:supplier ";
+		$query .= ", :room ";
+		$query .= ", :purchaseDate ";
+		$query .= ", :warrantyPeriod ";
+		$query .= ", :notice ";
+		$query .= ", :manufacturer ";
+		$query .= ", sysdate() ";
+	
+		$statement = $this->db->prepare($query);
+		$statement->bindparam(':companyname', $Component->getCompanyname());
+		$statement->bindparam(':street', $Component->getStreet());
+		$statement->bindparam(':zipcode', $Component->getZipcode());
+		$statement->bindparam(':city', $Component->getCity());
+		$statement->bindparam(':phone', $Component->getPhone());
+		$statement->bindparam(':mobile', $Component->getMobile());
+		$statement->bindparam(':fax', $Component->getFax());
+		$statement->bindparam(':email', $Component->getEmail());
+		$statement->bindparam(':id', $Component->getId());
 		$statement->execute();
 	
 		if ($query == false)
@@ -234,5 +385,100 @@ class DbConnector
 	}
 	
 	
+	/**
+	 * Deletes Component informations.
+	 *
+	 * @param ???
+	 * @return false if an error occurs, otherwise true.
+	 */
+	public function deleteComponent(Component $Component)
+	{
+		$query = "UPDATE komponent ";
+		$query .= "SET " . DB_MANAGE_VALID . " = 0";
+		$query .= ", " . DB_MANAGE_LASTUPDATED . " = sysdate() ";
+		$query .= "WHERE " . DB_COMPONENT_ID . " = :id ";
+		$query .= "AND " . DB_MANAGE_VALID . " = 1";
+	
+		$statement = $this->db->prepare($query);
+		$statement->bindparam(':id', $Component->getId());
+		$statement->execute();
+	
+		if ($query == false)
+			return false;
+	}
+	
+	/**
+	 * Updates Room informations.
+	 *
+	 * @param ???
+	 * @return false if an error occurs, otherwise true.
+	 */
+	public function updateRoom(Room $Room)
+	{
+		$query = "UPDATE raeume ";
+		$query .= "SET " . DB_ROOM_NAME . " = :name ";
+		$query .= ", " . DB_ROOM_NOTE . " = :note ";
+		$query .= ", " . DB_ROOM_NUMBER . "= :number ";
+		$query .= ", " . DB_MANAGE_LASTUPDATED . " = sysdate() ";
+		$query .= "WHERE " . DB_ROOM_ID . " = :id ";
+		$query .= "AND " . DB_MANAGE_VALID . " = 1";
+	
+		$statement = $this->db->prepare($query);
+		$statement->bindparam(':name', $Room->getName());
+		$statement->bindparam(':note', $Room->getNote());
+		$statement->bindparam(':id', $Room->getId());
+		$statement->execute();
+	
+		if ($query == false)
+			return false;
+	}
+	
+	/**
+	 * Creates Room informations.
+	 *
+	 * @param ???
+	 * @return false if an error occurs, otherwise true.
+	 */
+	public function createRoom(Room $Room)
+	{
+		$query = "INSERT INTO raeume ";
+		$query .= "" . DB_ROOM_NAME . " ";
+		$query .= ", " . DB_ROOM_NOTE . " ";
+		$query .= ", " . DB_MANAGE_CREATED . " ";
+		$query .= "VALUES (:name ";
+		$query .= ", :note ";
+		$query .= ", sysdate() ";
+	
+		$statement = $this->db->prepare($query);
+		$statement->bindparam(':name', $Room->getName());
+		$statement->bindparam(':note', $Room->getNote());
+		$statement->bindparam(':id', $Room->getId());
+		$statement->execute();
+	
+		if ($query == false)
+			return false;
+	}
+	
+	/**
+	 * Deletes Room informations.
+	 *
+	 * @param ???
+	 * @return false if an error occurs, otherwise true.
+	 */
+	public function deleteRoom(Room $Room)
+	{
+		$query = "UPDATE raeume ";
+		$query .= "SET " . DB_MANAGE_VALID . " = 0";
+		$query .= ", " . DB_MANAGE_LASTUPDATED . " = sysdate() ";
+		$query .= "WHERE " . DB_ROOM_ID . " = :id ";
+		$query .= "AND " . DB_MANAGE_VALID . " = 1";
+	
+		$statement = $this->db->prepare($query);
+		$statement->bindparam(':id', $Component->getId());
+		$statement->execute();
+	
+		if ($query == false)
+			return false;
+	}
 }
 ?>
