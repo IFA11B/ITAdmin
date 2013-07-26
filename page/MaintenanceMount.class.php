0c94d9073315eaaa1a2a1f3477ac94259e1a44a5
SeiresS <keckchris@web.de>
2013-07-26 08:27:29 +0200

    Files added

---
+++ page/MaintenanceMount.class.php
@@ -0,0 +1,21 @@
+<?php
+class MaintenanceChange implements Page
+{
+	function getTemplate()
+	{
+		return 'maintenanceMount.tpl';
+	}
+
+	function getContent()
+	{
+		return array(
+				pageTitle => 'Einbau'
+		);
+	}
+
+	static function getName()
+	{
+		return 'Einbauen';
+	}
+}
+?>
