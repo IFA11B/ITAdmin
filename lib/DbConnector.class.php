07555883973a30e8a5d76d0a94a2db926d31d555
Julian <julian.dinges@gmail.com>
2013-07-26 09:01:32 +0200

    Added rudimentary error handling to DbConnector

--- lib/DbConnector.class.php
+++ lib/DbConnector.class.php
@@ -193,20 +193,30 @@ class DbConnector
 		$DbUser = "entwickler";
 		$DbPass = "entwickler12";
 
-		// Open connection to mysql database (using PDO)
-		$this->db = new PDO(
-			sprintf("mysql:host=%s;dbname=%s;charset=utf8", $DbHost, $DbName),
-			$DbUser,
-			$DbPass,
-			array(
-				// Do not emulate prepared statements
-				// (see: http://stackoverflow.com/questions/10113562/pdo-mysql-use-pdoattr-emulate-prepares-or-not)
-				PDO::ATTR_EMULATE_PREPARES => false,
-
-				// If an error occurs, throw an exception.
-				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-			)
-		);
+		try
+		{
+    		// Open connection to mysql database (using PDO)
+    		$this->db = new PDO(
+                sprintf("mysql:host=%s;dbname=%s;charset=utf8", $DbHost, $DbName),
+                $DbUser,
+                $DbPass,
+                array(
+                    // Do not emulate prepared statements
+                    // (see: http://stackoverflow.com/questions/10113562/pdo-mysql-use-pdoattr-emulate-prepares-or-not)
+                    PDO::ATTR_EMULATE_PREPARES => false,
+    
+                    // If an error occurs, throw an exception.
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        
+                    // Set timeout to 3 seconds
+                    PDO::ATTR_TIMEOUT => "3"
+                )
+            );
+        }
+        catch (Exception $e)
+        {
+            die("Fehler bei der Verbindung zur Datenbank.");
+        }
 		
 	}
 
