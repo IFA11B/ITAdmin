fb17cd1752fc2c29483b0afe2208bbdbe7a580d5
Lukas Bagaric <deaeod@gmail.com>
2013-07-28 21:41:52 +0200

    extracted user info from DbConnector.class.php

--- config.php
+++ config.php
@@ -6,4 +6,9 @@ define('LIB_DIR', HOME_DIR . 'lib/');
 define('PAGE_DIR', HOME_DIR . 'page/');
 define('SMARTY_DIR', LIB_DIR . 'smarty/');
 
+define('CONFIG_DB_HOST', 'localhost');
+define('CONFIG_DB_NAME', 'itv_v2');
+define('CONFIG_DB_USER', 'entwickler');
+define('CONFIG_DB_PASS', 'entwickler12');
+
 ?>
